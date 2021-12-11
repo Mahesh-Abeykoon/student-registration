a33b7ab4f8f54c862b2a388aae7618a80d6a20e8
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-12-11 22:32:34 +0530

    Update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors({ origin: true, credentials: true, useFindAndModify: false }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello !")); // this is need not to initial
+app.get("/", (req, res) => res.send("Hello world!")); // this is need not to initial
 
 
 
